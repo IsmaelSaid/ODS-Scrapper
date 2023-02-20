5457c4bd504bcd380138ba3c9f36d32d6b257129
Ismael <ismael.saidpro@protonmail.com>
2023-02-20 19:34:47 +0400

    Adding the possibility to create plots from aggregated metrics

--- ScrappingPackage/opendatasoftvisualisations.py
+++ ScrappingPackage/opendatasoftvisualisations.py
@@ -1,6 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import pandas as pd
 
 class Opendatasoftvisualisations:
     """
@@ -58,3 +58,53 @@ class Opendatasoftvisualisations:
         if(save):
             plt.savefig(filename,format = "pdf",bbox_inches = "tight")
     
+class Opendatasoftvisualisations_aggregated:
+    def __init__(self, mhs_metrics : dict, ratio_metrics : dict):
+        self.mhs_metrics = mhs_metrics
+        self.ratio_metrics = ratio_metrics
+
+
+    def bar_plot_mhs_metrics(self, save : bool = False,title:str = "bar_plot_mhs_metric",filename = "bar_plot_mhs_metrics.pdf"):
+         #  retrieval of results
+
+        mhs_metrics_df = pd.DataFrame({
+            "portails" : list(self.mhs_metrics.keys()),
+            "nb_dataset/personn" : list(self.mhs_metrics.values())
+        })
+        
+        # feel and look
+        sns.set_style("whitegrid")
+
+        fig, ax = plt.subplots()
+        ax = sns.barplot(data=mhs_metrics_df,y="portails",ax = ax,x="nb_dataset/personn",palette=sns.color_palette('crest',1))
+        
+        plt.title(title)
+        plt.xticks(
+            rotation = 90
+        )
+
+        # save the plot somewhere
+        if(save):
+            plt.savefig(filename,format = "pdf",bbox_inches = "tight")
+
+    def bar_plot_ratio_metrics(self, save : bool = False,title:str = "bar_plot_ratio_metric",filename = "bar_plot_ratio_metrics.pdf"):
+            #  retrieval of results
+
+        mhs_metrics_df = pd.DataFrame({
+            "portails" : list(self.ratio_metrics.keys()),
+            "fed/unfed" : list(self.ratio_metrics.values())
+        })
+        
+        # feel and look
+        sns.set_style("whitegrid")
+
+        fig, ax = plt.subplots()
+        ax = sns.barplot(data=mhs_metrics_df,y="portails",ax = ax,x="fed/unfed",palette=sns.color_palette('crest',1))
+        
+        plt.title(title)
+        plt.xticks(
+            rotation = 90
+        )
+        # save the plot somewhere
+        if(save):
+            plt.savefig(filename,format = "pdf",bbox_inches = "tight")
